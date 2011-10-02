6268bf819d5fb0f6d6e1c6a94ff5ff64ff69de7c
Alan Franzoni <username@franzoni.eu>
2011-10-02 12:28:34 +0100

    Enh: basic xstream based repository implementation.

--- src/main/java/eu/franzoni/quickrepo/repository/XStreamRepository.java
+++ src/main/java/eu/franzoni/quickrepo/repository/XStreamRepository.java
@@ -1,8 +1,42 @@
 package eu.franzoni.quickrepo.repository;
 
+import com.google.common.io.Files;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+
+import java.io.File;
+
 public class XStreamRepository<T> {
+    private XStream xStream = new XStream(new StaxDriver());
+    private ByteArrayRepo diskRepo;
 
+    public XStreamRepository(File persistenceDir) {
+        this.diskRepo = new ByteArrayRepo(persistenceDir);
+    }
 
+    public void save(String id, T obj) {
+        this.diskRepo.save(id, this.xStream.toXML(obj).getBytes());
+
+    }
+    
+    public void saveOrUpdate(String id, T obj) {
+        this.diskRepo.saveOrUpdate(id, this.xStream.toXML(obj).getBytes());
+
+    }
+    
+    public void update(String id, T obj) {
+        this.diskRepo.update(id, this.xStream.toXML(obj).getBytes());
+        
+    }
+    
+    public void delete(String id) {
+        this.diskRepo.delete(id);
+        
+    }
+    
+    public T load(String id) {
+        return (T) this.xStream.fromXML(new String(this.diskRepo.load(id)));
+    }
 
 
 }
--- src/test/java/eu/franzoni/quickrepo/repository/XStreamRepositoryTest.java
+++ src/test/java/eu/franzoni/quickrepo/repository/XStreamRepositoryTest.java
@@ -1,13 +1,38 @@
 package eu.franzoni.quickrepo.repository;
 
+import com.google.common.io.Files;
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XStreamRepositoryTest extends TestCase {
-    public void setUp() throws Exception {
+    private File myTempDir;
+    private XStreamRepository<List<String>> repo;
 
+    @Before
+    public void setUp() throws Exception {
+        myTempDir = Files.createTempDir();
+        repo = new XStreamRepository<List<String>>(myTempDir);
     }
 
+    @After
     public void tearDown() throws Exception {
+        Files.deleteRecursively(this.myTempDir);
+    }
 
+    @Test
+    public void testAround() {
+        List<String> data = new ArrayList<String>();
+        data.add("ciao");
+        data.add("mamma");
+        this.repo.save("something", data);
+        List<String> retrieved = this.repo.load("something");
+        Assert.assertEquals(data, retrieved);
     }
 }
