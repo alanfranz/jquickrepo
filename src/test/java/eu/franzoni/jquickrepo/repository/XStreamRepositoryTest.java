027e2142a8d216524bad915cc97951d68b197c77
"Alan Franzoni ext:(%22) <username@franzoni.eu>
2013-10-16 17:47:42 +0200

    Test coverage

--- src/test/java/eu/franzoni/jquickrepo/repository/XStreamRepositoryTest.java
+++ src/test/java/eu/franzoni/jquickrepo/repository/XStreamRepositoryTest.java
@@ -3,26 +3,74 @@ package eu.franzoni.jquickrepo.repository;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class XStreamRepositoryTest {
+
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder();
 
-
-    private File myTempDir;
-    private MarshallingRepository<List<String>> repo;
-
     @Test
-    public void testAround() {
-        repo = new MarshallingRepository<List<String>>(tempFolder.getRoot());
+    public void savePersistsIfItemDoesNotExist() {
+        MarshallingRepository<List<String>> repo = new MarshallingRepository<List<String>>(tempFolder.getRoot());
         List<String> data = new ArrayList<String>();
         data.add("ciao");
         data.add("mamma");
-        this.repo.save("something", data);
-        List<String> retrieved = this.repo.load("something");
-        Assert.assertEquals(data, retrieved);
+        repo.save("something", data);
+        List<String> load = repo.load("something");
+        Assert.assertEquals(load, data);
+    }
+
+    @Test(expected = DuplicateResourceIdException.class)
+    public void saveFailsIfItemExists() {
+        MarshallingRepository<List<String>> repo = new MarshallingRepository<List<String>>(tempFolder.getRoot());
+        List<String> data = new ArrayList<String>();
+        data.add("ciao");
+        data.add("mamma");
+        repo.save("something", data);
+        repo.save("something", data);
+    }
+
+    @Test
+    public void saveOrUpdateAlwaysPersists() {
+        MarshallingRepository<List<String>> repo = new MarshallingRepository<List<String>>(tempFolder.getRoot());
+        List<String> data = new ArrayList<String>();
+        data.add("ciao");
+        data.add("mamma");
+        repo.saveOrUpdate("something", data);
+        data.add("forever");
+        repo.saveOrUpdate("something", data);
+        List<String> load = repo.load("something");
+        Assert.assertEquals(load, data);
+    }
+
+    @Test(expected = MissingIdException.class)
+    public void updateFailsIfItemDoesNotExist() {
+        MarshallingRepository<List<String>> repo = new MarshallingRepository<List<String>>(tempFolder.getRoot());
+        List<String> data = new ArrayList<String>();
+        data.add("ciao");
+        data.add("mamma");
+        repo.update("something", data);
+    }
+
+    @Test
+    public void updatePersistsIfItemExists() {
+        MarshallingRepository<List<String>> repo = new MarshallingRepository<List<String>>(tempFolder.getRoot());
+        List<String> data = new ArrayList<String>();
+        data.add("ciao");
+        data.add("mamma");
+        repo.save("something", data);
+        data.add("forever");
+        repo.update("something", data);
+        List<String> load = repo.load("something");
+        Assert.assertEquals(load, data);
+    }
+   
+
+    @Test(expected = UnknownResourceIdException.class)
+    public void loadFailsIfElementDoesNotExist() {
+        MarshallingRepository<List<String>> repo = new MarshallingRepository<List<String>>(tempFolder.getRoot());
+        repo.load("something");
     }
 }
